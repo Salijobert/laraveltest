1a6649fdf54535569d0e367a4c362146cbd3caef
Salijobert <santoschristianlloydlaxa@gmail.com>
2025-10-27 21:07:51 +0800

    added about, index, login

---
+++ laravel/resources/views/login.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login</title>
+</head>
+<body>
+  <h1>Login Page</h1>
+  <form>
+    <label>Email:</label>
+    <input type="email" name="email"><br>
+    <label>Password:</label>
+    <input type="password" name="password"><br>
+    <button type="submit">Login</button>
+  </form>
+
+  <a href="/">Home</a> |
+  <a href="/about">About</a>
+</body>
+</html>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index'); // loads resources/views/index.blade.php
+});
+
+Route::get('/about', function () {
+    return view('about'); // loads resources/views/about.blade.php
+});
+
+Route::get('/login', function () {
+    return view('login'); // loads resources/views/login.blade.php
 });
